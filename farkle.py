3d0b8ef996daa2fd0126c4554290f4b07236c97a
revnelson <Jollyman10>
2017-03-12 04:28:44 +0800

    Rejected commit: Default

--- farkle.py
+++ farkle.py
@@ -2,7 +2,7 @@
 
 from random import randint
 
-# Verification functions
+# Verification functions and point calculation
 from verify import *
 
 
@@ -13,7 +13,7 @@ order = []  # Maintain order of players as input
 # Input player's names and create scoreboard
 def players():
     for i in range(verify_players()):
-        name = input("Player %s's name: " % (i+1))
+        name = input("Player %s's name: " % (i+1))  # TODO Check input for error
         while True:
             if name not in scoreboard:
                 scoreboard[name] = 0
@@ -21,7 +21,7 @@ def players():
                 break
             else:
                 name = input("%s has already been added to the game.\nPlease give player %s a unique name: "
-                             % (name, i+1))
+                             % (name, i+1))  # TODO Check input for errors
     return scoreboard
 
 
@@ -30,58 +30,76 @@ def roll(n):
     return [str(randint(1, 6)) for _ in range(n)]
 
 
-def keep_or_finish():
-    selection = int(input("Please pick from the following options:\n1. Hold some dice and roll again.\n"
-                          "2. End turn and keep points\n"))
-    # TODO Add selection verification
-    return selection
-
-
-def hand():
+# Play hand
+def hand(player, score):
     dice = 6
-    # How many rolls this hand
-    roll_count = 0
-    # Dice set aside
-    set_aside = {}
+    set_aside = []  # Dice set aside
+
     while True:
-        roll_count += 1
+
         current_roll = roll(dice)
-        print(' '.join(current_roll))
-        # Verify roll
-        if verify_roll(current_roll, set_aside, roll_count):
-            # Insert action selection
-            action = keep_or_finish()
-            if action == 1:
-                # Keep some dice
-                keep = verify_keep(current_roll)
-                # Update number of dice available to roll
-                dice -= len(keep)
-                set_aside[roll_count] = keep
-                if dice == 0:
-                    print("Congratulations, you get to roll them all again!")
-                    return [sum([x for x in set_aside.values()][0]), True]  # todo calculate real scores
-            else:
-                # End turn and keep points
-                # todo make sure player has at least 1000 points to finish turn voluntarily
-                set_aside[roll_count] = [int(x) for x in current_roll]
-                return [sum([x for x in set_aside.values()][0]), False]  # todo calculate real scores
+        print("You just rolled:\n%s\n" % ' '.join(current_roll))
+
+        # Verify roll has value and check if all-dice
+        verified_roll = point_count([], current_roll)
+        if len(verified_roll[1]) is 0:
+            print("Congratulations, you get to roll them all again!")
+            return [point_count(set_aside, current_roll)[0], 'all']
+        if verified_roll[0] > 0:
+
+            # If current roll has value
+            print(point_count(set_aside, current_roll)[0])  # TODO Be more informative about scoring
+
+            while True:
+
+                action = verify_user_input()  # Ask player what they want to do
+
+                if action == 1:
+
+                    keep = verify_keep(current_roll)  # Keep some dice
+
+                    set_aside.append(keep)  # Add kept dice to list
+
+                    dice -= len(keep)  # Update number of dice available to roll
+
+                    break
+
+                else:
+                    # End turn and keep points
+                    hand_result = point_count(set_aside, current_roll)
+                    if scoreboard[player] < 1000 and hand_result[0] + score < 1000:  # Check for min score for stopping
+                        print("You must end your turn with at least 1000 points to get on the board.\n")
+                    else:
+                        return hand_result[0], 'done'  # End hand as well as turn
         else:
             print("Sorry, you didn't roll any points.")
-            return [0, False]  # If no points rolled
+            return [0, 'fail']  # If no points rolled, end turn with no points
 
 
-# todo Create turn system in order
+# TODO Create turn system in order
+
+# Starts the turn
 def turn(player):
-    # Starts the turn
+
+    score = 0
+
     while True:
 
-        hand_result = hand()
-        score = hand_result[0]
-        if hand_result[1] is False:
+        hand_result = hand(player, score)  # Passes name and accumulated turn score, returns points and ending reason
+
+        score += hand_result[0]
+
+        if hand_result[1] is 'done':  # Finished turn and adds points to scoreboard if player successful
             scoreboard[player] += score
             print(scoreboard)
             break
 
-# todo Make last turn scenario
+        elif hand_result[1] is 'fail':  # Finishes turn with no points if player failed
+            print(scoreboard)
+            break
+
+        print("Your score for this turn so far is:\n%s" % score)  # Loops if all dice in hand had value
+
+# TODO Make last turn scenario
 players()
 print(turn('John'))
--- verify.py
+++ verify.py
@@ -1,35 +1,105 @@
 def verify_players():
-    return int(input("How many players? "))
+    return int(input("How many players? "))  # TODO Check input for errors
 
 
-def verify_roll(dice, set_aside, roll_count):
-    if '1' in set(dice) or '5' in set(dice):
-        return True
-    possibilities = len(dice)
-    for x in dice:
-        if x not in "15": # Not a 1 or a 5
-            if dice.count(x) < 3: # Less than 3
-                # for value in set_aside.values(): # Check current hand
-                #     if str(value).count(x) >= 3:
-                #         return True
-                possibilities -= 1
-    if possibilities == 0:
-        return False
-    return True
-
-
+# Gets verified choice of dice to keep from current roll
 def verify_keep(current_roll):
-    # todo Add point value verification
-    keep = input("Which would you like to hold? ")
+
+    keep = ''.join(input("Which would you like to hold? ").split())
+
     while True:
+
         if keep.isdigit():
-            for i in keep:
+            for i in keep:  # Reports error if player chooses dice that aren't in the current roll
                 if keep.count(i) > current_roll.count(i):
                     keep = ''.join(input("Those weren't all in your roll.\n"
                                          "Please enter only numbers from your current roll of %s.\n"
-                                         "Which would you like to hold? " % (' '.join(current_roll))))
-        else:
-            keep = ''.join(input("You entered non-numbers.\nPlease enter only numbers from your current roll of %s.\n"
-                                 "Which would you like to hold? " % (' '.join(current_roll))))
+                                         "Which would you like to hold? " % (' '.join(current_roll))).split())
 
-        return [int(x) for x in keep]
+        elif keep.isdigit() is False:  # Reports error if player inputs non-digits
+            keep = ''.join(input("You entered non-numbers.\nPlease enter only numbers from your current roll of %s.\n"
+                                 "Which would you like to hold? " % (' '.join(current_roll))).split())
+
+        non_points = point_count([], [int(x) for x in keep])[1]
+
+        if len(non_points) > 0:  # Ensures all dice kept have value
+            keep = ''.join(input("The following dice have no value:\n%s\n"
+                                 "Please select dice with value: " % (' '.join([str(x) for x in non_points]))))
+        else:
+            break
+
+    return [int(x) for x in keep]
+
+
+# Gets verified player decision
+def verify_user_input():
+    user_input = input("Please pick a number from the following options:\n1. Hold some dice and roll again.\n"
+                       "2. End turn and keep points\n")
+    while True:
+        if user_input.isdigit():
+            if 0 < int(user_input) < 3:
+                return int(user_input)
+        user_input = input("You didn't enter a valid option.\nPlease pick a number from the following options:\n"
+                           "1. Hold some dice and roll again.\n"
+                           "2. End turn and keep points\n")
+
+
+# Main point calculating function
+def point_count(set_aside, current_roll):
+
+    points = 0
+
+    available_dice = set_aside + [[int(x) for x in current_roll]]
+
+    non_points = []
+
+    for roll in available_dice:
+
+        # Check for straights
+        if {1, 2, 3, 4, 5, 6} == set(roll):
+            points += 1000
+            roll = [x for x in roll if str(x) not in '123456']
+        if {1, 2, 3, 4, 5} == set(roll):
+            points += 500
+            [roll.remove(x+1) for x in range(5)]
+
+        pair_count = 0
+
+        for di in roll:
+
+            # Calculate multiples
+            if str(roll).count(str(di)) >= 3:
+                multiple = 0
+                if di == 1:
+                    multiple += di*1000
+                else:
+                    multiple += di*100
+                # Double value for each di after 3
+                for x in range(roll.count(di)-3):
+                    multiple *= 2
+                points += multiple
+                roll = [x for x in roll if x != di]  # Remove all of current di occurrences from the roll
+
+            if roll.count(di) == 2:  # Count pairs in roll
+                pair_count += 1
+
+        if pair_count == 6:
+            points += 500
+            roll = []
+
+        for di in roll[:]:
+            for value in set_aside:  # Check current hand for previous multiples and remove them
+                # print(value)
+                if str(value).count(str(di)) >= 3:
+                    points += 100
+                    roll.remove(di)
+
+        for di in roll:
+            if di == 1:
+                points += 100
+            elif di == 5:
+                points += 50
+            else:
+                non_points.append(di)
+
+    return points, non_points
